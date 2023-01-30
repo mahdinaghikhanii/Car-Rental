006ee6be0e664b05842de3898b33f0d365d377b6
Mahdinaghikhani <mahdinkh7@gmail.com>
2023-01-30 17:23:21 +0330

    set multiLanguage application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:car/ui/home/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'ui/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'), // English
+          Locale('es'), // Spanish
+        ],
         debugShowCheckedModeBanner: false,
         title: 'Car Rental',
         theme: ThemeData(
