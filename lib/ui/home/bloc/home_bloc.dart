9b33b44e5038cc2a6e4cde667e976e77c1db2730
Mahdinaghikhani <mahdinkh7@gmail.com>
2023-02-10 20:32:47 +0330

    add Flutter_bloc package on project

---
+++ lib/ui/home/bloc/home_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<HomeEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
